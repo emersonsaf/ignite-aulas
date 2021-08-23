cddad2db07d791fd2681fa2608993d2e518933ce
emersonsaf <emersonfilho3@gmail.com>
2021-08-23 17:00:47 -0300

    configura webpack and add sass dependency

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,25 @@
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>LISTA DE REPOSITORIOS</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositorio
+          </a>
+        </li>
+        <li>
+          <strong>unform2</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar Repositorio
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
