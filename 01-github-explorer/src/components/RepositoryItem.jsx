b4152e7c9eb2d77848ba245d3f3f809a3f21a175
emersonsaf <emersonfilho3@gmail.com>
2021-08-24 08:33:24 -0300

    add fetch data repository and map component repositoryitem

--- 01-github-explorer/src/components/RepositoryItem.jsx
+++ 01-github-explorer/src/components/RepositoryItem.jsx
@@ -3,9 +3,9 @@
 export function RepositoryItem(props) {
     return(
         <li>
-          <strong>{props.repository?.name ?? 'Default'}</strong>
-          <p>{props.repository?.description ?? 'Default'}</p>
-          <a href={props.repository?.link}>
+          <strong>{props.repository?.name}</strong>
+          <p>{props.repository?.description}</p>
+          <a href={props.repository?.html_url}>
             Acessar Repositorio
           </a>
         </li>
--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,22 +1,28 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import { useState, useEffect, useLayoutEffect } from 'react';
+
 import '../styles/repositories.scss'
 
-const repository = {
-  name: 'Unformed',
-  description: 'only one repository',
-  link: 'http://www.github.com/emersonsaf'
-}
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>LISTA DE REPOSITORIOS</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository="unform2" />
-        <RepositoryItem repository="unform3" />
+        {
+          repositories.map((repository) => {
+            return <RepositoryItem key={repository.name} repository={repository} />
+          })
+        }
       </ul>
     </section>
   )
