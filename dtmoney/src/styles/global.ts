c868674928cb8980022449cdbbf8f271a821f3a5
emersonsaf <emersonfilho3@gmail.com>
2021-08-25 17:18:48 -0300

    add dashboard, summary and transactionsTable components and styled components

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyled = createGlobalStyle`
   :root {
     --background: #f8f2f5;
     --red: #e52e4d;
+    --green: #33cc95;
     --blue: #5429cc;
     
     --blue-light: #6933ff;
