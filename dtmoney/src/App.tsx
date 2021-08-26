10586d3745f46544f13177a4d52abc62bc261d7c
emersonsaf <emersonfilho3@gmail.com>
2021-08-26 07:53:50 -0300

    css implamentation of modal component

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,6 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyled } from "./styles/global";
-import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,17 +1,52 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
+import closeImg from '../../assets/close.svg'
+
 
 interface NewTransactionModalProps {
     isOpen: boolean,
     onRequestClose: () => void,
 }
 
-export function NewTransactionModal({isOpen, onRequestClose }:NewTransactionModalProps) {
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
     return (
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
-            <h1>Cadastrar Transação</h1>
+            <button
+                type="button"
+                onClick={onRequestClose}
+                className="react-modal-close"
+            >
+                <img src={closeImg} alt="Fechar modal" />
+            </button>
+
+            <Container>
+                <h2>Cadastrar Transação</h2>
+                <input
+                    placeholder="Titulo"
+                />
+
+                <input
+                    placeholder="Valor"
+                    type="number"
+                />
+
+                <input
+                    placeholder="Categora"
+                />
+
+                <button
+                    type="submit"
+                >
+                    Cadastrar
+                </button>
+
+            </Container>
+
         </Modal>
     )
 }
