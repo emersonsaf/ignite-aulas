73a3f57df8bf0476245928a21d83539edc2ebc90
emersonsaf <emersonfilho3@gmail.com>
2021-08-28 20:12:28 -0300

    refactor summary deposits, withdraws and total valor

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -9,21 +9,22 @@ import { Container } from "./styles";
 export function Summary() {
   const { transactions } = useContext(TransactionsContext);
 
-  const totalDeposit = transactions.reduce((acc, transaction) => {
+  const summary = transactions.reduce((acc, transaction) => {
     if (transaction.type === 'deposit') {
-      return acc + transaction.amount;
+      acc.deposits += transaction.amount;
+      acc.total += transaction.amount;
+    } else {
+      acc.withdraws -= transaction.amount;
+      acc.total -= transaction.amount;
     }
-    return acc;
-  }, 0);
 
-  const totalWithdraw = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'withdraw') {
-      return acc + transaction.amount;
-    }
     return acc;
-  }, 0);
+  }, {
+    deposits: 0,
+    withdraws: 0,
+    total: 0
+  })
 
-  const total = totalDeposit - totalWithdraw
 
   return (
     <Container>
@@ -37,7 +38,7 @@ export function Summary() {
         <strong> {new Intl.NumberFormat('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        }).format(totalDeposit)}</strong>
+        }).format(summary.deposits)}</strong>
       </div>
       <div>
         <header>
@@ -49,7 +50,7 @@ export function Summary() {
         <strong> {new Intl.NumberFormat('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        }).format(totalWithdraw)}</strong>
+        }).format(summary.withdraws)}</strong>
       </div>
       <div className="highlight-background">
         <header>
@@ -61,7 +62,7 @@ export function Summary() {
         <strong> {new Intl.NumberFormat('pt-BR', {
           style: 'currency',
           currency: 'BRL'
-        }).format(total)}</strong>
+        }).format(summary.total)}</strong>
       </div>
     </Container>
   )
