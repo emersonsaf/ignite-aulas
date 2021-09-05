79cb2358b7e15374b84c2945e342a869306d8ef7
emersonsaf <emersonfilho3@gmail.com>
2021-09-05 10:00:21 -0300

    add styles and components to project

--- ignewx/src/pages/index.tsx
+++ ignewx/src/pages/index.tsx
@@ -1,14 +1,28 @@
 import Head from 'next/head'
+import { Header } from '../components/Header'
+import { SubscribeButton } from '../components/SubscribeButton'
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Inicio | IG.News</title>
+        <title>Home | IG.News</title>
       </Head>
-      <h1>
-        Hello
-      </h1>
+      <Header />
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span>👏 hey, wellcome</span>
+          <h1>News About the <span>React</span> world.</h1>
+          <p>
+            Get Access to all publications <br />
+            <span>for $9.90 month </span>
+            <SubscribeButton />
+          </p>
+
+        </section>
+        <img src="/images/avatar.svg" alt="Girl codding" />
+      </main>
     </>
   )
 }
