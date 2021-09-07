e66b0030a0c23a5c45acab463eec40bc6aeeb303
emersonsaf <emersonfilho3@gmail.com>
2021-09-07 10:48:44 -0300

    finish subscribe and remove strife duplicity

--- ignewx/src/pages/api/subscribe.ts
+++ ignewx/src/pages/api/subscribe.ts
@@ -1,18 +1,52 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from 'faunadb'
 import { getSession } from 'next-auth/client'
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+type User = {
+    ref: {
+        id: string
+    }
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         const session = await getSession({ req })
 
-        const stripeCustumer = await stripe.customers.create({
-            email: session.user.email,
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('email_by_user'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
 
-        })
+        let customerId = user.data.stripe_customer_id
 
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+            })
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('user'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+            customerId = stripeCustomer.id
+        }
+        
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustumer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [{
