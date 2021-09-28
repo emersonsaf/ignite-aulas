4aacfb78fa574368271cc3fa3c44688bb6e2ceb7
emersonsaf <emersonfilho3@gmail.com>
2021-09-28 08:38:53 -0300

    Create a new Interface project using Chakra Ui and create form login

---
+++ dashgo/src/components/Form/Input.tsx
@@ -0,0 +1,26 @@
+import { Input as ChakraInput, FormLabel, FormControl, InputProps as ChakreInputProps } from '@chakra-ui/react'
+
+interface InputProps extends ChakreInputProps {
+  name: string,
+  label?: string,
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor="password"> {label} </FormLabel>}
+      <ChakraInput
+        name={name}
+        id={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{
+          bgColor: "gray.900"
+        }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  )
+}
