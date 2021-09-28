d6eea7109d45d96eb7e97adce388cd3484cec3e5
emersonsaf <emersonfilho3@gmail.com>
2021-09-28 10:52:14 -0300

    Add icons and avatar on Header component

--- dashgo/src/components/Header.tsx
+++ dashgo/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box , Avatar} from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 
 export default function Header() {
   return (
@@ -46,6 +46,49 @@ export default function Header() {
 
         />
         <Icon as={RiSearchLine} fontSize="28" />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+
+      >
+        <HStack
+          spacing='8'
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+
+
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>
+              Emerson Filho
+            </Text>
+            <Text
+              color="gray.300"
+              fontSize="small"
+            >
+              emerson@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar
+           size="md"  
+           nome="Emerson Filho"
+           src="https://github.com/emersonsaf.png"
+          />
+        </Flex>
       </Flex>
     </Flex>
   )
